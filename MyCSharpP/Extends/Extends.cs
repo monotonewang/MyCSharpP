5c874b1514c65783b364ce59669fa204848222a3
montotone <zgjxfuqingwang@gmail.com>
2017-02-06 10:55:24 +0800

    test

--- MyCSharpP/Extends/Extends.cs
+++ MyCSharpP/Extends/Extends.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//继承特性
 namespace MyCSharpP.Extends {
     class Extends {
         //static void Main(String[] args) {
